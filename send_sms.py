e6f3ada6eb39116cd60127e68bc61b55d607ae9a
nouripurmehran <nouripur.mehran@gmail.com>
2019-01-14 12:28:30 +0330

    send sms

---
+++ send_sms.py
@@ -0,0 +1,7 @@
+import requests
+
+send_url =" https://api.kavenegar.com/v1/567235414F68622B506B694C634D66552B6B78667952324F424142764A753772/sms/send.json "
+
+sms = {"receptor" : "09359606252", "message": "salaaaaam"}
+
+delivery = requests.post(send_url,sms)
