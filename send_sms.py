51682fd84dd5af82d8cc47d7450c76d7563791f4
nouripurmehran <nouripur.mehran@gmail.com>
2019-01-14 13:53:19 +0330

    pep + print result

--- send_sms.py
+++ send_sms.py
@@ -1,7 +1,8 @@
 import requests
 
-send_url =" https://api.kavenegar.com/v1/567235414F68622B506B694C634D66552B6B78667952324F424142764A753772/sms/send.json "
+send_url = "https://api.kavenegar.com/v1/567235414F68622B506B694C634D66552B6B78667952324F424142764A753772/sms/send.json"
 
-sms = {"receptor" : "09359606252", "message": "salaaaaam"}
+sms = {"receptor": "09359606252", "message": "salaaaaam"}
 
-delivery = requests.post(send_url,sms)
+delivery = requests.post(send_url, sms)
+print(delivery)
