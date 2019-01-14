ac46748acc0a997e8390bcbc71b11e5018554217
nouripurmehran <nouripur.mehran@gmail.com>
2019-01-14 14:04:20 +0330

    solved error of delivery

--- send_sms.py
+++ send_sms.py
@@ -4,5 +4,5 @@ send_url = "https://api.kavenegar.com/v1/567235414F68622B506B694C634D66552B6B786
 
 sms = {"receptor": "09359606252", "message": "salaaaaam"}
 
-delivery = requests.post(send_url, sms)
+delivery = requests.post(send_url, data=sms)
 print(delivery)
