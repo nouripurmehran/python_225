003d293e3e21902ff5422dfaac0de1cb7a81a371
nouripurmehran <nouripur.mehran@gmail.com>
2019-01-15 12:29:01 +0330

    defined error message

--- send_sms.py
+++ send_sms.py
@@ -5,4 +5,14 @@ send_url = "https://api.kavenegar.com/v1/567235414F68622B506B694C634D66552B6B786
 sms = {"receptor": "09359606252", "message": "salaaaaam"}
 
 delivery = requests.post(send_url, data=sms)
-print(delivery)
+
+delivery = str(delivery)
+
+if delivery == "<Response [200]>":
+    print("پیام تحویل داده شد.")
+elif delivery == "<Response [418]>":
+    print("اعتبار حساب شما کافی نیست . ")
+elif delivery == "<Response [414]>":
+    print("تعداد دریافت کننده ها بیشتر از حد مجاز است.")
+else:
+    print("خطایی رخ داده است.")
