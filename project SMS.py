e38afaf42836abdefba13135782ddaa841306bfd
nouripurmehran <nouripur.mehran@gmail.com>
2019-01-16 14:10:42 +0330

    edit message

--- project SMS.py
+++ project SMS.py
@@ -14,7 +14,7 @@ if today_time == last_day:
 
     send_url = "https://api.kavenegar.com/v1/567235414F68622B506B694C634D66552B6B78667952324F424142764A753772/sms/send.json"
 
-    sms = {"receptor": "09359606252", "message": "salaaaaam"}
+    sms = {"receptor": "09359606252", "message": "سلاااام فلان کار یادت نره !!!"}
 
     delivery = requests.post(send_url, data=sms)
 
