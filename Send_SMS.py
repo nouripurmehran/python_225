e1eb024efab5cbabfaf628135623f11ac3209879
nouripurmehran <nouripur.mehran@gmail.com>
2019-01-19 15:48:45 +0330

    read date,message,phonenumber from file

--- Send_SMS.py
+++ Send_SMS.py
@@ -1,33 +1,32 @@
 import requests
 import datetime
-import calendar
+
 
 now = datetime.datetime.now()
 today_time = datetime.date.today()
 
-cy = now.year
-cm = now.month
+text = open("text", "r")
 
-last_day = calendar.monthrange(cy, cm)[1]
+for line in text:
+    x = line.split(",")
 
-if today_time == last_day:
+    if today_time == x[0]:
 
-    send_url = "https://api.kavenegar.com/v1/567235414F68622B506B694C634D66552B6B78667952324F424142764A753772/sms/send.json"
+        send_url = "https://api.kavenegar.com/v1/567235414F68622B506B694C634D66552B6B78667952324F424142764A753772/sms/send.json"
 
-    sms = {"receptor": "09359606252", "message": "سلاااام فلان کار یادت نره !!!"}
+        sms = {"receptor": x[2] , "message": x[1]}
+        delivery = requests.post(send_url, data=sms)
 
-    delivery = requests.post(send_url, data=sms)
+        delivery = str(delivery)
 
-    delivery = str(delivery)
+        if delivery == "<Response [200]>":
+            print("پیام ارسال شد.")
+        elif delivery == "<Response [418]>":
+            print("اعتبار حساب شما کافی نیست . ")
+        elif delivery == "<Response [414]>":
+            print("تعداد دریافت کننده ها بیشتر از حد مجاز است.")
+        else:
+            print("خطایی رخ داده است.")
 
-    if delivery == "<Response [200]>":
-        print("پیام ارسال شد.")
-    elif delivery == "<Response [418]>":
-        print("اعتبار حساب شما کافی نیست . ")
-    elif delivery == "<Response [414]>":
-        print("تعداد دریافت کننده ها بیشتر از حد مجاز است.")
     else:
-        print("خطایی رخ داده است.")
-
-else:
-    print("today is not last day of month")
+        print("do not have any remind for this date")
